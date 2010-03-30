90b1f7735bc6d5a22ddb5f9c8068d50e08818c48
Kim Brugger <kb468@MGPC17.(none)>
2010-03-30 14:07:58 +0100

    and it now works

--- block_stacker.pl
+++ block_stacker.pl
@@ -15,7 +15,9 @@ my $START = 0;
 my $END   = 0;
 my $LEVEL = 0;
 
-my @data = ([100,150], [90,91], [95,97], [92,94], [87,89], [98,99], [175,180], [155, 170], [200,250]);
+my @data = ([100,150], [98,101], [87,89], [175,180], [155, 170], [200,250], [185,190]);
+
+@data = ([100,150], [200,250], [300, 350], [180,181], [165, 170],  [285,290], [155,156]);
 
 my @stack = shift @data;
 
@@ -33,41 +35,43 @@ foreach my $data ( @data ) {
   my ( $left, $right ) = (0, int(@stack));
   my $middle = floor(($right - $left)/2);
 
-  print "MID :: $middle ( $left, $right)\n";
-  print " --> $$data[0], $$data[1]\n";
+#  print "MID :: $middle ( $left, $right)\n";
+#  print " --> $$data[0], $$data[1]\n";
 #  print Dumper( \@stack );
-  print "ARRAY:: ". join( "" ,(map { " (@$_)" } @stack )) ."\n";
+  print "::: ". join( "" ,(map { " (@$_)" } @stack )) ."\n";
   
   while (1) {
+
+    print " $left --> $right || $middle ($$data[0],$$data[1])\n";
     
     if ( $$data[ $END ] < $stack[ $middle ][$START] ) {
       if ( $middle == 0 ) {
-#	print "UNSHIFT\n";
 	unshift @stack, $data;
 	last;
       }
       elsif ( $$data[ $START ] > $stack[ $middle - 1 ][$END] ) {
-#	print "SPLICE \n";
 	splice @stack, $middle, 0, $data;
 	last;
       }
       else {
 	$right = $middle;
+	last if ( $right <= $left );
 	$middle = $left + floor(($right - $left)/2);
-#	print "MID2 :: $middle ($left, $right)\n";
-
       }
     }
     elsif ($$data[ $START ] > $stack[ $middle ][$END] ) {
       if ( $middle + 1 == @stack ) {
-#	print "PUSHING \n";
 	push @stack, $data;
 	last;
       }
+      elsif ( $$data[ $END ] < $stack [ $middle + 1 ][ $START ]) {
+	splice @stack, $middle + 1, 0, $data;
+	last;
+	      }
       else {
 	$left = $middle;
+	last if ( $right <= $left );
 	$middle = $left + floor(($right - $left)/2);
-#	print "MID3 :: $middle ($left, $right)\n";
       }
     }
   
@@ -79,4 +83,4 @@ foreach my $data ( @data ) {
 
 
 #print Dumper(\@stack);
-print "SORTED ARRAY:: ". join( "" ,(map { " (@$_)" } @stack )) ."\n";
+print "::: ". join( "" ,(map { " (@$_)" } @stack )) ."\n";

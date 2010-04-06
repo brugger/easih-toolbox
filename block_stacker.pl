dbeeb443520aab50596617aa7bf967f5686e3b7b
Kim Brugger <kim.brugger@easih.ac.uk>
2010-04-06 15:37:06 +0100

    sort and build is two times faster than the native algorithm

--- block_stacker.pl
+++ block_stacker.pl
@@ -11,128 +11,165 @@ no warnings 'recursion';
 use Data::Dumper;
 use POSIX qw(ceil floor);
 
+use lib '/home/kb468/perllib/';
+use EASIH::Timer;
+my $timer = EASIH::Timer->New();
+
 
 my $START = 0;
 my $END   = 1;
 my $SUB   = 2;
 
 
-my $VERBOSE_LEVEL = 00;
+my $VERBOSE_LEVEL = 0;
 
 my @test_data = ([100,150], [98,101], [87,89], [175,180], [155, 170], [200,250], [185,190]);
 
 @test_data = ([100,150], [200,250], [300, 350], [180,181], [165, 170],  [285,290], [155,156]);
-@test_data = ([100,150], [50,95], [90,110], [90, 105],  );
+@test_data = ([100,150], [50,95], [90,110], [90, 105]  );
 
 my @stack;# = shift @test_data;
 
-#my $stack;
-
 my $dprint = 2;
-#my $counter = 520;
 
-use Time::HiRes;
-
-my $t0 = Time::HiRes::gettimeofday();
+$timer->start();
+my ($identical, $contained, $overlapped, $pushed) = (0,0,0, 0);
 
 print "-"x60 . "\n";
 print " "x25 . "START RUN\n";
 print "-"x60 . "\n";
 
-
 if (1) {
-  my $counter = 0;
-  while(<>) {
-    chomp;
-    print "STATUS: $counter entries, array length: " . @stack ." \n" if ( $counter++ % 10000 == 0);
-    
-    b_insert(\@stack, [split("\t", $_)]);
+
+  if (1) {
+    my $features;
+    while(<>) {
+      chomp;
+      
+      next if (/^\z/);
+      push @$features, [split("\t", $_)];
+    }
+    b_inserts_sorted(\@stack, $features, 1);
+  }
+  else {
+    my $counter = 0;
+    while(<>) {
+      chomp;
+       print "STATUS: $counter entries, array length: " . @stack ." \n" if ( $counter++ % 10000 == 0);
+      
+      b_insert(\@stack, [split("\t", $_)]);
 #    print_stack( \@stack, 0 );
 #    print "--------------------------\n\n";
- #   $dprint++ if ($counter >= 60);
+      #   $dprint++ if ($counter >= 60);
 #    $VERBOSE_LEVEL = 10 if ($counter >= 1051);
 #    last if ( ! $counter--);
+    }
   }
 }
 else {
-  b_inserts(\@stack, \@test_data);
+  b_inserts_sorted(\@stack, \@test_data, 1);
 }
 
-my $t1 = Time::HiRes::gettimeofday(); 
+$timer->stop();
+
+my $fragment_cutoff = 0;
 
 print "-"x60 . "\n";
 print " "x25 . "ENDED RUN\n";
-print " "x20 . "RUNTIME :: ". int(( $t1 - $t0)/60 ) . " min\n";
+print " "x20 . "RUNTIME :: ".( $timer->report('s'))."\n";
 print "-"x60 . "\n";
-print_stack( \@stack, 100 );
+#print_stack( \@stack, $fragment_cutoff);
 
-my($nodes, $max_depth, $troughs) = stack_stats( \@stack );
+
+my($nodes, $max_depth, $troughs) = stack_stats( \@stack, $fragment_cutoff );
 
 print "Stack stats :: nodes= $nodes, max depth= $max_depth, troughs= $troughs \n";
-
-# 
-# 
-# 
-# Kim Brugger (01 Apr 2010)
-sub stack_stats {
-  my ( $stack_ref, $level, $max_depth, $troughs  ) = @_;
-
-  my $nodes = 0;
-  $max_depth ||= 1;
-  $level     ||= 1;
-  $troughs   ||= 0;
-
-  $max_depth = $level if ( $level > $max_depth);
-
-  foreach my $field ( @$stack_ref ) {    
-    my $cutoff = 100;
-    next if ( $$field[ $END ] - $$field[ $START ] + 1 < $cutoff);
-    
-    $troughs++ if ( $level == 1);
-
-    $nodes++;
-
-    if ( @{$$field[ $SUB ]} > 0 ) {
-      my ($sub_nodes, $l_max_depth) = stack_stats($$field[ $SUB ], $level + 1, $max_depth);
-
-      $nodes += $sub_nodes;
-      $max_depth = $l_max_depth if ( $max_depth < $l_max_depth);
-    }
-    
-  }
-  
-  $troughs--;
-
-  return( $nodes, $max_depth, $troughs);
-}
+verbose("S2: $identical, $contained, $overlapped, $pushed \n", 0);
 
 
 # 
-# 
+# Simple wrapper for inserting multiple data fields.
 # 
 # Kim Brugger (31 Mar 2010)
-sub print_stack {
-  my ( $stack_ref, $cutoff, $level ) = @_;
+sub b_inserts_sorted {
+  my ( $stack, $input_data, $sort ) = @_;
 
-  $level ||= 1;
+  # the data is not sorted here so we sort it 
+  if ( $sort ) {
+    print "start sorting ... \n";
+    @$input_data = sort {$$a[ $START ] <=> $$b[ $START ] || $$b[ $END ] <=> $$a[ $END ] } @$input_data;
+    print "done \n";
 
-  foreach my $field ( @$stack_ref ) {    
+  }
 
-    next if ( $$field[ $END ] - $$field[ $START ] + 1 < $cutoff);
-
-    print "|"x$level . "-$$field[ $START ]->$$field[ $END ] (".($$field[ $END ] - $$field[ $START ] + 1).")\n";
-    if ( @{$$field[ $SUB ]} > 0 ) {
-      print "|"x$level."\\\n";
-      print_stack( $$field[ $SUB ], $cutoff, $level + 1);
-      print "|"x$level."/\n";
-    }
+  my $counter = 0;
+  foreach my $data ( @$input_data ) {
+    verbose("STATUS: $counter entries, array length: " . @stack ." \n", 0) if ( $counter++ % 10000 == 0);
+    verbose("S2: $identical, $contained, $overlapped, $pushed \n", 0)  if ( ($counter - 1) % 10000 == 0) ;
+    b_insert_sorted( $stack, $data);
   }
   
 }
 
 
-
 # 
+# 
+# 
+# Kim Brugger (30 Mar 2010)
+sub b_insert_sorted {
+  my ( $stack_ref, $data) = @_;
+
+  verbose("algorithm :: b_insert_sorted", 1);
+
+  # initialise the sub array value so we can use it as a reference later, otherwise the 
+  # information is lost.
+  $$data[ $SUB ] = [] if ( ! $$data[ $SUB ]);
+
+
+  # If the stack_ref is empty, just push the first element in and lets get on with it.
+  if ( !$stack_ref || !@$stack_ref  ) {
+    verbose("Up front push \n", 2);
+    push @$stack_ref, $data;
+    return $stack_ref;
+  }
+  
+  verbose("data: $$data[ $START ] $$data[ $END ] <=> block: $$stack_ref[ -1 ][$START] $$stack_ref[ -1 ][$END]\n", 1);
+
+  if ( $$data[ $START  ] == $$stack_ref[ -1 ][ $START ]  &&
+       $$data[ $END    ] == $$stack_ref[ -1 ][ $END   ] ) {
+
+    verbose("IDENTICAL BLOCK", 2);
+    $identical++;
+    $$data[ $SUB ] = $$stack_ref[ -1 ][$SUB];
+    $$stack_ref[ -1 ][$SUB] = [$data];
+  }
+  elsif ( $$data[ $START  ] >= $$stack_ref[ -1 ][ $START ]  &&
+	  $$data[ $END    ] <= $$stack_ref[ -1 ][ $END   ] ) {
+    
+    verbose("CONTAINED BLOCK", 2);
+    $contained++;
+    b_insert_sorted( $$stack_ref[ -1 ][$SUB], $data);
+  }
+  # New block overlaps to the right of the middle block
+  elsif ( $$data[ $START   ] >= $$stack_ref[ -1 ][$START]  &&
+	  $$data[ $START   ] <= $$stack_ref[ -1 ][$END]  ) {
+    verbose("JOIN TWO BLOCKS RIGHT\n", 2);
+    $overlapped++;
+    b_insert( $$stack_ref[ -1 ][$SUB], [$$data[ $START ], $$stack_ref[ -1 ][$END]]);
+    $$stack_ref[ -1 ][$END] = $$data[ $END ];
+  }
+  # There is a gap between the end block and this one. Just push it on the end of  the array!
+  else {
+    $pushed++;
+    push @$stack_ref, $data;
+  }
+   
+  return;
+}
+
+
+
+#
 # Simple wrapper for inserting multiple data fields.
 # 
 # Kim Brugger (31 Mar 2010)
@@ -140,7 +177,7 @@ sub b_inserts {
   my ( $stack, $input_data ) = @_;
   
   foreach my $data ( @$input_data ) {
-    $stack = b_insert( $stack, $data);
+    b_insert( $stack, $data);
   }
   
 }
@@ -154,17 +191,8 @@ sub b_inserts {
 sub b_insert {
   my ( $stack_ref, $data ) = @_;
 
-  if ( 1 && $dprint ){
-#    verbose("STACK_REF : ". Dumper( $stack_ref ), 2);
-#    verbose("INDATA: ". Dumper( $data ), 2);
-  }
 
  RERUN:
-
-#  $$stack_ref[ 0 ][ $SUB ] = [] if ( @$stack_ref == 1 && ! $$stack_ref[ 0 ][ $SUB ] );
-
-
-  
 
   # initialise the sub array value so we can use it as a reference later, otherwise the 
   # information is lost.
@@ -175,8 +203,6 @@ sub b_insert {
   if ( !$stack_ref || !@$stack_ref  ) {
     verbose("Up front push \n", 2);
     push @$stack_ref, $data;
-#    print Dumper( $stack_ref );
-#    print Dumper( \@stack );
     return $stack_ref;
   }
   
@@ -184,10 +210,6 @@ sub b_insert {
   # the middle of the array
   my ( $left, $right ) = (0, int(@$stack_ref));
   my $middle = floor(($right - $left)/2);
-    
-#  verbose("MIDDLE $middle ( $left, $right)\n", 1) if ( $dprint);
-#  verbose("BLOCK  $$data[0], $$data[0]\n", 1) if ( $dprint);
-#    print "::: ". join( "" ,(map { " (@$_)" } @$stack_ref )) ."\n";
 
   # Flush the buffer constantly
   $| = 1;
@@ -394,5 +416,66 @@ sub verbose {
   $message =~ s/\n+\Z//g;
   print "MESS " . ":"x$level . " $message\n";
 #  sleep 1;
+}
+
+
+
+# 
+# 
+# 
+# Kim Brugger (01 Apr 2010)
+sub stack_stats {
+  my ( $stack_ref, $cutoff,  $level, $max_depth, $troughs  ) = @_;
+
+  my $nodes = 0;
+  $max_depth ||= 1;
+  $level     ||= 1;
+  $troughs   ||= 0;
+
+  $max_depth = $level if ( $level > $max_depth);
+
+  foreach my $field ( @$stack_ref ) {    
+    next if ( $$field[ $END ] - $$field[ $START ] + 1 < $cutoff);
+    
+    $troughs++ if ( $level == 1);
+
+    $nodes++;
+
+    if ( @{$$field[ $SUB ]} > 0 ) {
+      my ($sub_nodes, $l_max_depth) = stack_stats($$field[ $SUB ], $level + 1, $max_depth);
+
+      $nodes += $sub_nodes;
+      $max_depth = $l_max_depth if ( $max_depth < $l_max_depth);
+    }
+    
+  }
+  
+  $troughs--;
+
+  return( $nodes, $max_depth, $troughs);
+}
+
+
+# 
+# 
+# 
+# Kim Brugger (31 Mar 2010)
+sub print_stack {
+  my ( $stack_ref, $cutoff, $level ) = @_;
+
+  $level ||= 1;
+
+  foreach my $field ( @$stack_ref ) {    
+
+    next if ( $$field[ $END ] - $$field[ $START ] + 1 < $cutoff);
+
+    print "|"x$level . "-$$field[ $START ]->$$field[ $END ] (".($$field[ $END ] - $$field[ $START ] + 1).")\n";
+    if ( @{$$field[ $SUB ]} > 0 ) {
+      print "|"x$level."\\\n";
+      print_stack( $$field[ $SUB ], $cutoff, $level + 1);
+      print "|"x$level."/\n";
+    }
+  }
+  
 }
 

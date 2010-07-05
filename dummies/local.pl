0fbbd7a94aa378d2ef8dd4f655870a283790a891
Kim Brugger <kim.brugger@easih.ac.uk>
2010-07-05 13:19:43 +0100

    added control flags so it will crash, run longer etc

--- dummies/local.pl
+++ dummies/local.pl
@@ -9,16 +9,25 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+use Getopt::Std;
+
+
+my %opts;
+getopts('S:f:F', \%opts);
+
+
 my $sleep_time = shift || 10;
-my $fail_rate = 1;
+my $fail_rate = $opts{f} || 5;
 
 srand;
 $sleep_time = int(rand( $sleep_time ));
 
+$sleep_time = $opts{'S'} if ($opts{'S'});
+
 #print "Starting sleep for $sleep_time seconds\n";
 sleep($sleep_time);
 
-if ( $fail_rate > int(rand(100))) {
+if ( $opts{'F'} || $fail_rate > int(rand(100))) {
   print "------- Failed -----------\n";
   exit 1;
 }

e16671a1a749d8ff6cdae110000e9de908607698
Kim Brugger <kim.brugger@easih.ac.uk>
2011-01-06 11:01:15 +0000

    bla bla bla

--- devel/snp_module_test.pl
+++ devel/snp_module_test.pl
@@ -14,6 +14,14 @@ use EASIH::SNPs;
 
 print "Hellow world, let look at some snps...\n";
 
+print Dumper(EASIH::SNPs::phylop_score_GRCh37(10, 60007));
+print Dumper(EASIH::SNPs::phylop_score_GRCh37(10, 6000));
+print Dumper(EASIH::SNPs::phylop_score_GRCh37(10, 60002));
+print Dumper(EASIH::SNPs::phylop_score_GRCh37(10, 60009));
+print Dumper(EASIH::SNPs::phylop_score_GRCh37(10, 60037));
+
+exit;
+
 print Dumper(EASIH::SNPs::fetch_snp_hg18(1, 536205));
 print Dumper(EASIH::SNPs::fetch_snp_GRCh37(1, 536205));
 

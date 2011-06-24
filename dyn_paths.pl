61bfa05dec31e1af027819c003a3e928eeed0e57
Kim Brugger <kim.brugger@easih.ac.uk>
2011-06-24 12:50:50 +0100

    script that uses dynamic paths for more flexible code further on

---
+++ dyn_paths.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl 
+# 
+# Map/realign/indels/snps integrated pipeline
+# 
+# 
+# Kim Brugger (27 Jul 2010), contact: kim.brugger@easih.ac.uk
+
+# Sets up dynamic paths for EASIH modules...
+# Makes it possible to work with multiple checkouts without setting 
+# perllib/perl5lib in the enviroment.
+my $DYNAMIC_LIB_PATHS = 1;
+BEGIN {
+  if ( $DYNAMIC_LIB_PATHS ) {
+    my $path = $0;
+    if ($path =~ /.*\//) {
+      $path =~ s/(.*)\/.*/$1/;
+      push @INC, "$path/modules" if ( -e "$path/modules");
+      $path =~ s/(.*)\/.*/$1/;
+      push @INC, "$path/modules" if ( -e "$path/modules");
+      
+    }
+    else {
+      push @INC, "../modules" if ( -e "../modules");
+      push @INC, "./modules" if ( -e "./modules");
+    }
+  else {
+    use lib '/home/kb468/easih-toolbox/modules/';
+  }
+}
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Std;
+
+use EASIH;
+
+print "PATH :: @INC\n";
+
+print "Using version : " . EASIH::Git::version() ."\n";
+
+
+use EASIH::Mail;
+
+EASIH::Mail::send('kim.brugger@easih.ac.uk', "From Dyn paths", "I am using the following paths: @INC\n");

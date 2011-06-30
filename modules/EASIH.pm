a373ae776e6d4f0e7d2a70f4a27ed8f9aaa894b0
Kim Brugger <kim.brugger@easih.ac.uk>
2011-06-30 11:29:07 +0100

    checkouts from dev

---
+++ modules/EASIH.pm
@@ -0,0 +1,19 @@
+package EASIH;
+# 
+# General catch all and configuration wrapper module for all EASIH scripts.
+# 
+# 
+# Kim Brugger (13 Jun 2011), contact: kim.brugger@easih.ac.uk
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+# Gives an error if it is not the master branch + gives access to version information.
+use EASIH::Git;
+
+
+1;
+
+
+

0bd578b88182f137ac6087c827d41ec9a333af5a
Kim Brugger <kim.brugger@easih.ac.uk>
2011-07-27 09:42:55 +0100

    tweaks and extensions of the DB and its API

--- DONE/tables.sql
+++ DONE/tables.sql
@@ -76,10 +76,19 @@ CREATE TABLE illumina_multiplex_stats (
   
 );
 
+
+CREATE TABLE instrument (
+
+  mid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name                VARCHAR(100) NOT NULL UNIQUE,
+
+  KEY name_idx (name)
+);
+
 CREATE TABLE run (
 
   rid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name                VARCHAR(100) NOT NULL ,
+  name                VARCHAR(100) NOT NULL UNIQUE,
   platform            VARCHAR(50),
 
   KEY name_idx (name)
@@ -95,6 +104,7 @@ CREATE TABLE file (
   rid		      INT NOT NULL,
 
   total_reads	      int,
+  read_length         int,
   sample_size	      int,
   Q30bases	      float,
   duplicates	      float,
@@ -107,13 +117,15 @@ CREATE TABLE file (
 
 CREATE TABLE mapping_stats (
 
-  fid1                INT NOT NULL PRIMARY KEY,
+  fid1                INT NOT NULL,
   fid2                INT,
   reference           VARCHAR(100) NOT NULL,
   unique_hits	      INT,
   non_unique_hits     INT,
   duplicates          INT,
   
+  PRIMARY KEY (fid1, reference),
+  KEY f1_idx (fid1),
   KEY f12_idx (fid1, fid2)
 );
 
@@ -126,6 +138,9 @@ CREATE TABLE qv_boxplot (
   q2		      float,
   q3		      float,
   q4		      float,
+  q5		      float,
+  q6		      float,
+  q7		      float,
   
   PRIMARY KEY (fid, x),
   KEY fid_idx( fid )
@@ -196,6 +211,16 @@ CREATE TABLE adaptors (
 );
 
 
-
-
-  
+DELETE FROM project;
+DELETE FROM sample;
+DELETE FROM file;
+DELETE FROM qv_boxplot;
+DELETE FROM qv_histogram;
+DELETE FROM base_distribution;
+DELETE FROM GC_distribution;
+DELETE FROM duplicates;
+DELETE FROM duplicated_seqs;
+DELETE FROM adaptors;
+DELETE FROM illumina_multiplex_stats;
+DELETE FROM illumina_lane_stats;
+DELETE FROM run;

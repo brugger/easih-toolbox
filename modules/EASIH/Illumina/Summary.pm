a10fc4d28a09f41c97061f1e3fb6b2d621abf78d
Kim Brugger <kim.brugger@easih.ac.uk>
2011-11-25 10:51:58 +0000

    finds the right path, hopefully, and some changes in identifiers

--- modules/EASIH/Illumina/Summary.pm
+++ modules/EASIH/Illumina/Summary.pm
@@ -5,13 +5,10 @@ package EASIH::Illumina::Summary;
 # 
 # Kim Brugger (17 Nov 2011), contact: kim.brugger@easih.ac.uk
 
-
 use strict;
 use warnings;
 use Data::Dumper;
 use XML::Simple;
-
-
 
 # 
 # 
@@ -19,6 +16,11 @@ use XML::Simple;
 # Kim Brugger (17 Nov 2011)
 sub readin_summaries {
   my ($indir) = @_;
+
+  # Trim the name back to the toplevel
+  $indir =~ s/Data.*//;
+  # and make sure that we are pointing to the right place.
+  $indir .= "/Data/reports/Summary/";
 
   opendir(DIR, "$indir");
   my @files = grep(!/^\./, sort readdir(DIR));
@@ -31,10 +33,10 @@ sub readin_summaries {
 
 #    print Dumper( $$config{Lane}{1} );
   
-foreach my $lane ( keys %{$$config{'Lane'}} ) {
+    foreach my $lane ( keys %{$$config{'Lane'}} ) {
 #    foreach my $lane ( 6 ) {
-      $res{$$config{'Read'}}{$lane}{'Phas'}  = $$config{'Lane'}{$lane}{Phasing};
-      $res{$$config{'Read'}}{$lane}{'Preph'} = $$config{'Lane'}{$lane}{Prephasing};
+      $res{$$config{'Read'}}{$lane}{'phas'}  = $$config{'Lane'}{$lane}{Phasing};
+      $res{$$config{'Read'}}{$lane}{'preph'} = $$config{'Lane'}{$lane}{Prephasing};
       
       $res{$$config{'Read'}}{$lane}{'FirstCycleInt'}   = $$config{'Lane'}{$lane}{FirstCycleIntPF};
       $res{$$config{'Read'}}{$lane}{'FirstCycleIntSD'} = $$config{'Lane'}{$lane}{FirstCycleIntPFSD};
@@ -51,11 +53,11 @@ foreach my $lane ( keys %{$$config{'Lane'}} ) {
       $res{$$config{'Read'}}{$lane}{'PrcPFClusters'}   = $$config{'Lane'}{$lane}{PrcPFClusters};
       $res{$$config{'Read'}}{$lane}{'PrcPFClustersSD'} = $$config{'Lane'}{$lane}{PrcPFClustersSD};
       
-      $res{$$config{'Read'}}{$lane}{'Clusters'}   = sprintf("%d",$$config{'Lane'}{$lane}{ClustersRaw}*$$config{densityRatio});
-      $res{$$config{'Read'}}{$lane}{'ClustersSD'} = sprintf("%d",$$config{'Lane'}{$lane}{ClustersRawSD}*$$config{densityRatio});
+      $res{$$config{'Read'}}{$lane}{'clusters'}   = sprintf("%d",$$config{'Lane'}{$lane}{ClustersRaw}*$$config{densityRatio});
+      $res{$$config{'Read'}}{$lane}{'clustersSD'} = sprintf("%d",$$config{'Lane'}{$lane}{ClustersRawSD}*$$config{densityRatio});
       
-      $res{$$config{'Read'}}{$lane}{'ClustersPF'}   = sprintf("%d",$$config{'Lane'}{$lane}{ClustersPF}*$$config{densityRatio});
-      $res{$$config{'Read'}}{$lane}{'ClustersPFSD'} = sprintf("%d",$$config{'Lane'}{$lane}{ClustersPFSD}*$$config{densityRatio});
+      $res{$$config{'Read'}}{$lane}{'clustersPF'}   = sprintf("%d",$$config{'Lane'}{$lane}{ClustersPF}*$$config{densityRatio});
+      $res{$$config{'Read'}}{$lane}{'clustersPFSD'} = sprintf("%d",$$config{'Lane'}{$lane}{ClustersPFSD}*$$config{densityRatio});
     }
     
   }
